b6012d4d5056e080b68c8efa2b3539c908552c6c
zoro <547848098@qq.com>
2022-12-08 14:39:31 +0800

    first commit idea

---
+++ src/com/inntra/idea/HelloIDEA.java
@@ -0,0 +1,14 @@
+package com.inntra.idea;
+
+/**
+ * ClassName:HelloIDEA
+ * Package:com.inntra.idea
+ * Description:
+ *
+ * @Author:zoro
+ * @create:2022/12/7 - 15:35
+ * @Version:v1.0
+ */
+public class HelloIDEA {
+
+}
