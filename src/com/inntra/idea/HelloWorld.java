3458f4824150564ff2056de354aaf147fbb6d5f5
zoro <547848098@qq.com>
2022-12-08 14:53:11 +0800

    second commit idea

--- src/com/inntra/idea/HelloWorld.java
+++ src/com/inntra/idea/HelloWorld.java
@@ -3,5 +3,6 @@ package com.inntra.idea;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("second hello");
     }
 }
